3a16b3a66c81f0fca25ebda72f3a93b7f9f6dc51
黑寒冰 <47298124+Icetebe@users.noreply.github.com>
2019-04-08 22:45:36 +0800

    Add files via upload

---
+++ ICELauncher/Core/Auth/OfflineAuthenticator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ICELauncher.Core.Net.MojangApi.Api;
+
+namespace ICELauncher.Core.Auth
+{
+    public static class OfflineAuthenticator
+    {
+        public static Tuple<string, Uuid> DoAuthenticate(string displayname)
+        {
+            Uuid uuid = new Uuid()
+            {
+                PlayerName = displayname,
+                Value = Guid.NewGuid().ToString("N")
+            };
+
+            string accessToken = Guid.NewGuid().ToString("N");
+
+            return new Tuple<string, Uuid>(accessToken, uuid);
+        }
+    }
+}
